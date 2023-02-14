5157fbdbc540c84e0052f412daf26504a87316a0
rekishinn <lexn.barros@outlook.com>
2023-02-13 21:05:24 -0300

    add

---
+++ Exercicios DESCOMPLICA/2A - Programação Extrema/StringArrays_exemplo003.java
@@ -0,0 +1,26 @@
+public class StringArrays_exemplo003 {
+    public static void main(String[] args){
+       
+        int[][] matriz = new int[2][3]; // linhas e colunas
+        int i, j;
+
+        matriz[0][0] = 1;
+        matriz[0][1] = 2;
+        matriz[0][2] = 3;
+
+        matriz[1][0] = 4;
+        matriz[1][1] = 5;
+        matriz[1][2] = 6;
+
+        for(i=0 ; i<2 ; i++){
+            for(j=0 ; j<3 ; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
+
+    }
+    
+}
+       
+
+
