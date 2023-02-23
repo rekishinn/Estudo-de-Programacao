9f9a9a4ae9e99c4b47d4ef3f55e44f1e4f71b4c3
rekishinn <lexn.barros@outlook.com>
2023-02-23 19:02:38 -0300

    new

--- Exercicios DESCOMPLICA/2A - Programação Extrema/StringBuilder_exemplo001/src/StringBuilder_exemplo001.java
+++ Exercicios DESCOMPLICA/2A - Programação Extrema/StringBuilder_exemplo001/src/StringBuilder_exemplo001.java
@@ -1,7 +1,21 @@
 public class StringBuilder_exemplo001 {
     public static void main(String[] args) {
-
-        kkeita
         
+        String nome = "Oswaldo";
+        String sobreNome = "Borges";
+        String nomeCompleto = nome + sobreNome;
+        String nomeComBuffer;
+
+        System.out.println(nomeCompleto);
+
+
+        StringBuilder buffer = new StringBuilder();
+
+        buffer.append(nome);
+        buffer.append(sobreNome);
+
+        nomeComBuffer = buffer.toString();
+
+        System.out.println(nomeComBuffer);
     }
 }
