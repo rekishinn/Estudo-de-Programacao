b5a2f2d1bd579e0be94871e364d41fc31e77a44a
rekishinn <lexn.barros@outlook.com>
2023-03-09 16:30:19 -0300

    new

---
+++ Exercicios DESCOMPLICA/2A - Programação Extrema/excessões_exemplo001.java
@@ -0,0 +1,16 @@
+public class excessões_exemplo001{
+    public static void main(String[] args){
+        
+       try {
+
+        int resultado = 10 / 0; //dovido por 0 dá erro, logo o programa fecharia automaticamente
+        System.out.println(resultado);
+       }
+
+       catch (Exception e) { //pra isso o catch vai pegar o erro e printar na tela, sem fechar o programa.
+        System.out.println("Erro: " + e.getMessage()); //print: Erro: / by zero
+
+       }
+        
+    }
+}
---
+++ Exercicios DESCOMPLICA/2A - Programação Extrema/excessões_exemplo003.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class excessões_exemplo003{
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
+        do{
+            System.out.println("Digite o número 1 ou 2:");
+            System.out.println();
+            try{
+                opcao = teclado.nextInt();
+                switch(opcao){
+                    case 1:
+                        System.out.println("Você digitou a opção " + opcao);
+                        break;
+                    case 2:
+                        System.out.println("Você digitou a opção " + opcao);
+                    default:
+                    throw new Excecao("Erro"); //throw força a mostrar o erro
+                }
+            }
+            catch(Excecao excecao){
+                System.out.println(excecao.getMessage());
+            }
+            finally {
+                System.out.println("---------------");
+            }
+        }while(true);
+        
+    }
+}
