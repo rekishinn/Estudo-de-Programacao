26200c8587c88043b11cc9bb0e2069f022312313
rekishinn <lexn.barros@outlook.com>
2023-03-30 16:54:30 -0300

    fix

--- Cursos Complementares/BeeCrowd - 1000/BeeCrowd_1003.java
+++ Cursos Complementares/BeeCrowd - 1000/BeeCrowd_1003.java
@@ -1,29 +1,31 @@
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
+ 
 /**
  * IMPORTANT: 
  *      O nome da classe deve ser "Main" para que a sua solução execute
  *      Class name must be "Main" for your solution to execute
  *      El nombre de la clase debe ser "Main" para que su solución ejecutar
  */
-public class BeeCrowd_1002 {
+public class BeeCrowd_1003 {
  
     public static void main(String[] args) throws IOException {
-        
-        double raio, A, π;
-        π = 3.14159;
-        A = 0;
-
-        Locale.setDefault(Locale.US);
-        
+ 
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite o valor do raio: ");
-        raio = input.nextFloat();
-        
-        A = π*(Math.pow(raio, 2));
 
-        System.out.printf("A=%.4f%n",A);
+        int soma, A, B;
+
+        System.out.println("Digite o valor de A:");
+        A = input.nextInt();
+        
+        System.out.println("Digite o valor de B:");
+        B = input.nextInt();
+
+        soma = A + B;
+        System.out.printf("SOMA = " + soma + "%n");
+
         input.close();
+ 
     }
+ 
 }
