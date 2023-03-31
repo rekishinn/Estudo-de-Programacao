ad5e386c6e27e165cb82d9b3b9612587b3e1ee57
rekishinn <lexn.barros@outlook.com>
2023-03-31 17:11:20 -0300

    new

---
+++ Cursos Complementares/LinkedIn Learning - Fundamentos da Programação/Capitulo 5 - Funções/funcoes.js
@@ -0,0 +1,9 @@
+function somarNumeros(a, b) {
+    //var a = 5;
+    //var b = 10;
+
+    var soma = a + b;
+    return soma;
+    }
+//poderá ser executada em outro arquivo, pois o index.html lê primeiro as funcoes
+//(e sim, a ordem importa lá no codigo) e depois lê o script, tudo em conjunto.
---
+++ Cursos Complementares/LinkedIn Learning - Fundamentos da Programação/Capitulo 5 - Funções/script.js
@@ -0,0 +1,6 @@
+var primeiro = prompt("Digite o primeiro valor: ");
+var segundo = prompt("Digite o segundo valor: ");
+
+var resultado = somarNumeros(parseInt(primeiro), parseInt(segundo)); // return faz com q "somarNumero()" seja substituido pelo valor da var soma
+
+alert("o valor da soma é: " + resultado);
