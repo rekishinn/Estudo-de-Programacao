ee4ef19540a6ad3f9b258600283573ed756ef59c
rekishinn <lexn.barros@outlook.com>
2023-04-24 20:15:37 -0300

    fix

---
+++ Cursos Complementares/LinkedIn Learning - Fundamentos da Programação/Capitulo 1 - Primeiro programa (1)/script.js
@@ -0,0 +1,4 @@
+alert("Olá mundo!"); //exibe na tela do usuário o texto em forma de popup
+
+alert("Olá \"mundo!\""); // o uso da \ antes de um caractere especial faz com que ele obrigatoriamente seja mostrado
+alert("Olá mundo \nBom dia!") // \n faz uma quebra de linha
---
+++ Cursos Complementares/LinkedIn Learning - Fundamentos da Programação/Capitulo 1 - Primeiro programa (1)/script2.js
@@ -0,0 +1,3 @@
+var nome = prompt("Qual é o seu nome?"); //abre uma caixa para o usuário preencher, em forma de popup
+
+alert("Olá " + nome + "!") //exibe na tela do usuário
